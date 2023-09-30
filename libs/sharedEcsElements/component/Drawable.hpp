095b8610c2b1c68ad12c6f62aab7f62e27af374a
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2023-09-30 15:51:41 +0200

    feat(Drawable): add Drawable component (#46)
    * feat(Drawable): add Drawable component
    
    * refactor(component::drawable): add const "NO TEXTURE" default value

---
+++ libs/sharedEcsElements/component/Drawable.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-NAN-5-1-rtype-lucas.hauszler [WSL : Ubuntu]
+** File description:
+** Drawable
+*/
+
+#pragma once
+
+#include <string>
+
+namespace component
+{
+    struct Drawable {
+        const static std::string NO_TEXTURE;
+
+
+        Drawable(const std::string &textureName_ = Drawable::NO_TEXTURE, float scale_ = 1)
+            : textureName(textureName_), scale(scale_) {}
+        ~Drawable() = default;
+
+        Drawable(const Drawable &other) = default;
+        Drawable &operator=(const Drawable &other) = default;
+
+        std::string textureName;
+        float scale;
+    };
+
+    const std::string Drawable::NO_TEXTURE = "NO TEXTURE";
+}
