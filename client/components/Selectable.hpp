c9566ea120ed43d59706daf536b509b983f126fd
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2023-10-10 14:09:49 +0200

    feat(component): add selectable (#133)

---
+++ client/components/Selectable.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-NAN-5-1-rtype-lucas.hauszler [WSL : Ubuntu]
+** File description:
+** Selectable
+*/
+
+#pragma once
+
+#include <functional>
+
+namespace rtype::component
+{
+    /**
+     * @brief Selectable component used to store the selected state of an entity
+    */
+    struct Selectable
+    {
+        Selectable(bool selected_ = false) : selected(selected_) {};
+        ~Selectable(void) = default;
+
+        Selectable(const Selectable &other) = default;
+        Selectable &operator=(const Selectable &other) = default;
+
+        bool selected;
+    };
+}
